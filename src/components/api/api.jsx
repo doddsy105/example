6767f267cb1068e5535dd52d743bd6fc6a6f102f
Adam Dodds <adam@Adams-MacBook-Pro.local>
2018-08-24 12:02:50 +0100

    add bits and bobs

--- src/components/api/api.jsx
+++ src/components/api/api.jsx
@@ -14,22 +14,32 @@ class Results extends Component {
   onSearchChange(e) {
     this.setState({ inputValue: e.target.value});
     let Location = this.state.inputValue;
-    const url = 'https://www.rentalcars.com/FTSAutocomplete.do?solrIndex=fts_en&solrRows=%7b5%7d&solrTerm=%7b' + Location + '%7d';
-    fetch(url)
-      .then(res => res.json())
-      .then(res => {
-        this.setState({ data: res.results.docs });
-        return res;
-      })
-      .catch(err => {
-        console.log('Error happened during fetching!', err);
-      });
-    
-    console.log(this.state.inputValue);
+
+    if(this.state.inputValue.length >= 2) {
+      const url = 'https://www.rentalcars.com/FTSAutocomplete.do?solrIndex=fts_en&solrRows=%7b5%7d&solrTerm=%7b' + Location + '%7d';
+      fetch(url)
+        .then(res => res.json())
+        .then(res => {
+          this.setState({ data: res.results.docs });
+          return res;
+        })
+        .catch(err => {
+          console.log('Error happened during fetching!', err);
+        });
+    }
+
+    if(this.state.inputValue.length <= 1 ) {
+      this.setState({ data: [] });
+    }
+
+    if(this.state.inputValue.length === 0 && this.state.inputValue.length < 1 ) {
+      this.setState({ data: ['No search Results'] });
+    }
   }
 
 
   render() {
+    console.log(this.state.inputValue.length);
     console.log(this.state.data);
     return (
       <div>
